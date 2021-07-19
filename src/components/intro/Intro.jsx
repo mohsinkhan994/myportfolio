89bb40f3fed771cd0bbd69a6aa1dc2641f7dc0d1
mohsinkhan994 <mkhan5332@gmail.com>
2021-07-20 00:33:04 +0530

    added code for intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,39 @@
-import "./intro.scss"
+import "./intro.scss";
+import { init} from 'ityped';
+import { useEffect, useRef } from "react";
 
-export default function intro() {
+export default function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, {
+            backDelay:1500,
+            backSpeed:60,
+            showCursor:true,
+            strings: [ "React" , "Java"],
+
+        });
+       
+    }, []);
+
     return (
         <div className = "intro" id ="intro"> 
-            
+        <div className="left">
+            <div className="imgContainer">
+                <img src= "assets/me.jpeg" alt = ""/>
+            </div>
+        </div>
+        <div className="right">
+            <div className="wrapper">
+                <h2>Hi There, I'm </h2>
+                <h1>Mohsin Khan</h1>
+                <h3>Software Developer - <span ref={textRef}></span></h3>
+            </div>
+            <a href="#portfolio">
+                <img src= "assets/down.png" alt=""/>
+            </a>
+            </div>            
         </div>
     )
 }
