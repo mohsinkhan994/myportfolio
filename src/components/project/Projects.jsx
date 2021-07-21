c6e8704dfbce81ae914fcb32e9bd099bce841ad0
mohsinkhan994 <mkhan5332@gmail.com>
2021-07-22 00:14:17 +0530

    added code for projects

--- src/components/project/Projects.jsx
+++ src/components/project/Projects.jsx
@@ -1,33 +1,61 @@
-import "./project.scss"
+import "./project.scss";
+import { useState } from "react";
 
 export default function Projects() {
+
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    const data = [{
+        id: "1",
+        img: "https://mma.prnewswire.com/media/562209/ALLISON_TRANSMISSION_INC__LOGO.jpg?p=publish",
+        projectName: "Project- Extranet Admin",
+        projectDescription: "This project is of Allison Transmission, which is an American manufacturer of commerical automatic transmission and hybrid propulsion systems.",
+        technology: "Technical Plateform used- Java,Springboot 1.5, TFS, React,HTML, CSS, Tomcat,Maven and Hibernate.",
+        duration: "Period- Working on it from 5th January 2020 till present."
+
+    },
+    {
+        id: "2",
+        img: "assets/app.png",
+        projectName: "Project- Library Management System",
+        projectDescription: "This is centralized online library management system,where a student can easily search and issue the books available in college library.",
+        technology: "Technical Plateform used- Spring MVC, JSP, Javascript, HTML,CSS.",
+        duration: "Period- Made during my Post Graduation Diploma in Advanced Computing course."
+    }]
+
+    const handleClick = (way) => {
+        way === "left"
+            ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 1) :
+            setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+    }
+
     return (
-        <div className = "project" id="project"> 
-            <div className="slider">
-                <div className="container">
-                    <div className="item">
-                        <div className="left">
-        <div className="leftContainer">
-            <div className="imageContainer">
-                <img src= "https://mma.prnewswire.com/media/562209/ALLISON_TRANSMISSION_INC__LOGO.jpg?p=publish" alt=""></img>
-            </div>
-            {/* <h2>Allison Transmission</h2>
-            <div className="imgContainer1">
-                <img src ="https://mma.prnewswire.com/media/562209/ALLISON_TRANSMISSION_INC__LOGO.jpg?p=publish" alt=""/>
-       </div> */}
-             </div>
-                        </div>
-                        <div className="right">
-                            {/* <h1>Allison Transmission</h1> */}
-                            <h1>Project- Extranet Admin</h1>
-                            <p>This project is of Allison Transmission, which is an American manufacturer of commerical automatic transmission and hybrid propulsion systems.
-                            </p>
-                            <p>Technical Plateform used- Java,Springboot 1.5, TFS, React,HTML, CSS, Tomcat,Maven and Hibernate.</p>
-                            <p>Period- 5th January 2020 to present.</p>
+        <div className="project" id="project">
+            <div className="slider"
+                style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+            >
+                {data.map((d) => (
+                    <div className="container">
+                        <div className="item">
+                            <div className="left">
+                                <div className="leftContainer">
+                                    <div className="imageContainer">
+                                        <img src={d.img} alt="" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="right">
+                                <h1>{d.projectName}</h1>
+                                <p>{d.projectDescription}</p>
+                                <p>{d.technology}</p>
+                                <p>{d.duration}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
+                ))}
             </div>
+            <img src="assets/arrow.png" className="arrow left" alt="" onClick={() => handleClick("left")} />
+            <img src="assets/arrow.png" className="arrow right" alt="" onClick={() => handleClick()} />
         </div>
     )
 }
