3f24c1d31b572c67d0c46e80e482f56a91bdae45
mohsinkhan994 <mkhan5332@gmail.com>
2021-07-23 12:31:37 +0530

    added particle js

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,43 @@ import Contacts from "./components/contacts/Contacts";
 import Menu from "./components/menu/Menu";
 import "./app.scss";
 import {useState} from "react";
+import  Particles from "react-particles-js";
+
 
 function App() {
   const [menuOpen,setMenuOpen] = useState(false)
-
   return (
+    
     <div className="app">
+      <Particles 
+      params={{
+        "particles": {
+            "line_linked": {
+                        "color":"#FFFFFF"
+                        },
+            "number": {
+                "value": 50
+            },
+            "size": {
+                "value": 5
+            }
+        },
+        "interactivity": {
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "repulse"
+                }
+            }
+        }
+    }}
+    style={{
+            width: '100%',
+            background: `#000000` ,
+            
+
+     }}
+      />
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
      <div className="sections">
