899f2f9d588f4b9d5fb927f09b110ad2f013b484
Mohsin <mkhan5332@gmail.com>
2026-07-20 00:59:21 +0500

    removed unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
-import Topbar from "./components/topbar/Topbar";
-import Intro from "./components/intro/Intro";
-import Skills from "./components/skills/Skills";
-import Projects from "./components/project/Projects";
-import Contacts from "./components/contacts/Contacts";
-import Menu from "./components/menu/Menu";
+
 import "./app.scss";
 import {useState} from "react";
-import  Particles from "react-particles-js";
 import Portfolio from "./components/portfolio/Portfolio";
 
 
@@ -16,43 +10,7 @@ function App() {
     
     <div className="app">
         <Portfolio/>
-    {/*  <Particles className="particlesCanvas"*/}
-    {/*  params={{*/}
-    {/*    "particles": {*/}
-    {/*        "line_linked": {*/}
-    {/*                    "color":"#FFFFFF"*/}
-    {/*                    },*/}
-    {/*        "number": {*/}
-    {/*            "value": 50*/}
-    {/*        },*/}
-    {/*        "size": {*/}
-    {/*            "value": 5*/}
-    {/*        }*/}
-    {/*    },*/}
-    {/*    "interactivity": {*/}
-    {/*        "events": {*/}
-    {/*            "onhover": {*/}
-    {/*                "enable": true,*/}
-    {/*                "mode": "repulse"*/}
-    {/*            }*/}
-    {/*        }*/}
-    {/*    }*/}
-    {/*}}*/}
-    {/*style={{*/}
-    {/*        width: '100%',*/}
-    {/*        background: `#000000` ,*/}
-    {/*        */}
 
-    {/* }}*/}
-    {/*  />*/}
-    {/*  <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>*/}
-    {/*  <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>*/}
-    {/* <div className="sections">*/}
-    {/*   <Intro/>*/}
-    {/*   <Skills/>*/}
-    {/*   <Projects/>*/}
-    {/*   <Contacts/>*/}
-    {/* </div>*/}
     </div>
   );
 }
