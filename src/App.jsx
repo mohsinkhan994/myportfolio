bfa0269d98a45841f25920cb95d526efa165d86f
Mohsin <mkhan5332@gmail.com>
2026-07-20 09:39:41 +0500

    build failure fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-
 import "./app.scss";
-import {useState} from "react";
 import Portfolio from "./components/portfolio/Portfolio";
 
 
 function App() {
-  const [menuOpen,setMenuOpen] = useState(false)
   return (
     
     <div className="app">
